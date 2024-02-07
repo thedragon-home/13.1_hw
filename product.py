c1e9fe90449924470a05a48607cb22fb26a7e5a1
Abdumalikov Sardor <sardorlearnjs@gmail.com>
2024-02-07 22:19:05 +0700

    created ptoduct and category classes(task 1)

---
+++ product.py
@@ -0,0 +1,18 @@
+class Category:
+    '''
+    Category
+    '''
+    def __init__(self, name, description, goods):
+        self.name = name
+        self.description = description
+        self.goods = goods
+
+class Product:
+    '''
+    Product
+    '''
+    def __init__(self, name, descriotion, price, quontity_in_stock):
+        self.name = name
+        self.description = descriotion
+        self.price = price
+        self.quontity_in_stock == quontity_in_stock
