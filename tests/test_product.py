c5392b1cb5de5906e6562280148df03794d8b05c
Abdumalikov Sardor <sardorlearnjs@gmail.com>
2024-02-10 20:39:08 +0700

    "Режимы доступа"

--- tests/test_product.py
+++ tests/test_product.py
@@ -4,19 +4,18 @@ from main import Category, Product
 
 @pytest.fixture
 def category_example():
-    return Category('Fruits', 'different kind of fruits', ['apple', 'banana', 'orange'])
+    return Category("Electronics", "Category for electronic devices")
 
 @pytest.fixture
 def product_example():
-    return Product('Apple', 'red apples', '200', '10')
+    return Product.add_product("Laptop", "Powerful laptop", 1500.0, 10)
 
 def test_category(category_example):
-    assert category_example.name == 'Fruits'
-    assert category_example.description == 'different kind of fruits'
-    assert category_example.goods == ['apple', 'banana', 'orange']
+    assert category_example.name == 'Electronics'
+    assert category_example.description == 'Category for electronic devices'
 
 def test_product(product_example):
-    assert product_example.name == 'Apple'
-    assert product_example.description == 'red apples'
-    assert product_example.price == '200'
-    assert product_example.quontity_in_stock == '10'
+    assert product_example.name == 'Laptop'
+    assert product_example.description == 'Powerful laptop'
+    assert product_example.price == 1500.0
+    assert product_example.quontity_in_stock == 10
